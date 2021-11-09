494a9d8586648821b8dce111e25a485bf03e432f
HyunJun Baek <hjbaek91@gmail.com>
2021-11-10 02:10:25 +0900

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,30 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+// #1 Values and Variables
 
-40 + 8 + 23 - 10;
+// There are many different kinds of 'values' in JavaScript.
+// The most common values are 'strings' and 'numbers'.
+
 console.log(40 + 8 + 23 - 10);
+console.log("John");
+console.log(23);
+
+// We can use 'variables' to store values.
+// Variables are like boxes with labels that can be used to find the values whenever convenient.
+// By just changing the values, we can change the content within the variables.
+
+let firstName = "Steve";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// There are naming conventions for JavaScript variables.
+// It is encouraged to use 'camel-casing' when naming variables with more than one word.
+// Variable names should be descriptive.
+
+let myCurrentJob = "Programmer";
+let myPreviousJob = "Biochemist";
+
+let job1 = "programmer";
+let job2 = "biochemist";
+
+console.log(myCurrentJob);
