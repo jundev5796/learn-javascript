f310bb2f0f165f32971495fc33562285ce88bbfb
HyunJun Baek <hjbaek91@gmail.com>
2021-11-11 03:45:37 +0900

    Practice Problem #2

---
+++ Practice Problems/#2 Data Types/script.js
@@ -0,0 +1,15 @@
+// #2 Data Types
+
+let country = "Republic of Korea";
+let population = 52;
+
+// 1. Declare variables called 'isIsland' and set its value according to your country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet.
+// 2. Log the type of 'isIsland', 'population', 'country' and 'language' to the console.
+
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
