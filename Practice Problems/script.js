6fa18314bed1694fb07f8aa0ade4d5520f9e05ae
HyunJun Baek <hjbaek91@gmail.com>
2021-11-10 17:42:22 +0900

    Extra Materials + Practice Problem #1

---
+++ Practice Problems/script.js
@@ -0,0 +1,12 @@
+// #1 Values and Variables
+
+// 1. Declare variables called 'country', 'continent' and 'population' and assign their values according to your own country (population).
+// 2. Log their values to the console.
+
+let country = "Republic of Korea";
+let continent = "Asia";
+let population = 52;
+
+console.log(country);
+console.log(continent);
+console.log(population);
