a8a63855e137ba94087a000371202e0a1b74071c
HyunJun Baek <hjbaek91@gmail.com>
2021-11-12 03:19:03 +0900

    Practice Problem #3

---
+++ Practice Problems/#3 Let, Const, and Var/script.js
@@ -0,0 +1,17 @@
+// #3 Let, Const, and Var
+
+// 1. Set the value of 'language' to the language spoken where you live (some countries have multiple languages, but just choose one).
+// 2. Think about which variables should be const variables (which values will never change, and which might change?). Then change these variables to const.
+// 3. Try to change one of the changed variables now, and observe what happens.
+
+// let country = "Republic of Korea";
+// let continent = "Asia";
+// let population = 52;
+// let isIsland = false;
+// let language = Korean;
+
+const country = "Republic of Korea";
+const continent = "Asia";
+let population = 52;
+const isIsland = false;
+const language = "Korean";
