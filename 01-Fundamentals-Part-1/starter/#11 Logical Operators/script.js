1cb1c2051c5f64e1bf5705ae30a12ec48ac7090c
HyunJun Baek <hjbaek91@gmail.com>
2021-11-19 20:50:21 +0900

    #11 Logical Operators

---
+++ 01-Fundamentals-Part-1/starter/#11 Logical Operators/script.js
@@ -0,0 +1,29 @@
+// #11 Logical Operators
+
+// Ex)
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision); // true
+console.log(hasDriversLicense || hasGoodVision); // true
+console.log(!hasDriversLicense); // false
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Sarah is able to drive!");
+// } else {
+//   console.log("Someone else should drive...");
+// }
+
+// Using logical operators on multple conditions
+
+const isTired = false; // new variable
+console.log(hasDriversLicense || hasGoodVision || isTired); // true
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive!");
+} else {
+  console.log("Someone else should drive...");
+}
