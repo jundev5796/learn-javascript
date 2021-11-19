fa70c86809b36af3196c0016e92b7f8bf5115cb0
HyunJun Baek <hjbaek91@gmail.com>
2021-11-19 20:30:37 +0900

    #10 Boolean Logic

---
+++ 01-Fundamentals-Part-1/starter/#10 Boolean Logic/script.js
@@ -0,0 +1,17 @@
+// #10 Boolean Logic
+
+// Boolean logic uses 'true' and 'false' values to solve complex logical problems.
+// In this case, several logical operators are used at the same time.
+// This is not only specific to JavaScript, but also to many other languages.
+
+// Ex 1) (can be either true or false)
+// A: Sarah has a driver's license
+// B: Sarah has good vision
+
+// -AND- (When both values are TRUE)
+// Sarah has a driver's license 'AND' good vision --> True
+
+// -OR- (When only one value is TRUE)
+// Sarah has a driver's license 'OR' good vision --> True
+
+// -NOT- (Inverts true/false value)
