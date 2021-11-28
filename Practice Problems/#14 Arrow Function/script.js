fee76215e1d54e4bafd49f435d268e0c8902c950
HyunJun Baek <hjbaek91@gmail.com>
2021-11-28 16:48:35 +0900

    Practice Problem #14

---
+++ Practice Problems/#14 Arrow Function/script.js
@@ -0,0 +1,10 @@
+// #14 Arrow Function
+
+// 1. Recreate the last assignment, but this time create an arrow function called 'percentageOfWorld3'
+
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+
+const popSouthKorea3 = percentageOfWorld3(52);
+const popUnitedStates3 = percentageOfWorld3(330);
+const popFrance3 = percentageOfWorld3(67);
+console.log(popSouthKorea3, popUnitedStates3, popFrance3);
