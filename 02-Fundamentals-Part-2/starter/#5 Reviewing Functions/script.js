0eb78359d3f403f8810d34c1f37cc74b3ce43ebf
HyunJun Baek <hjbaek91@gmail.com>
2021-11-30 22:36:03 +0900

    #5 Reviewing Functions

---
+++ 02-Fundamentals-Part-2/starter/#5 Reviewing Functions/script.js
@@ -0,0 +1,21 @@
+// #16 Reviewing Functions
+
+// Calling other functions and applying if/else statements
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+};
+
+const yearUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    // if the 1st condition is satisfied, the return statement immediately exits from the code
+    return retirement;
+  } else {
+    return -1;
+  }
+};
+
+console.log(yearUntilRetirement(1995, "John"));
+console.log(yearUntilRetirement(1970, "Mike"));
