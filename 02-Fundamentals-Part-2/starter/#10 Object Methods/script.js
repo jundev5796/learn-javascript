4cca28943b300b238d0aa503280c22a9c1148675
HyunJun Baek <hjbaek91@gmail.com>
2021-12-10 17:30:18 +0900

    #10 Object Methods (Part 2)

--- 02-Fundamentals-Part-2/starter/#10 Object Methods/script.js
+++ 02-Fundamentals-Part-2/starter/#10 Object Methods/script.js
@@ -15,11 +15,33 @@ const john = {
   //   return 2037 - birthYear;
   // },
 
-  calcAge: function (birthYear) {
-    console.log(this);
-    return 2037 - this.birthYear;
+  // The 'this' keyword points to the object which calls the function.
+  // calcAge: function () {
+  // console.log(this);
+  //   return 2037 - this.birthYear;
+  // },
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    if (this.hadDriversLicense) {
+      return `${this.firstName} is a ${this.calcAge()}-year old ${
+        this.job
+      }, and he has ${this.hadDriversLicense ? "a" : "no"} driver's license`;
+    }
   },
 };
 
 console.log(john.calcAge()); // 42
-// console.log(john["calcAge"](1995)); // 42
+
+console.log(john.age);
+console.log(john.age);
+console.log(john.age);
+
+// -CHALLENGE-
+// "John is a 42-year old developer, and he has a driver's license"
+
+console.log(john.getSummary());
