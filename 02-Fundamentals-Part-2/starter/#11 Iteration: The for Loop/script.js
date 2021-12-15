98316acced24c31cc9639d5349e53a0eb73da0ce
HyunJun Baek <hjbaek91@gmail.com>
2021-12-15 16:26:57 +0900

    #11 Iteration: The for Loop (Final)

--- 02-Fundamentals-Part-2/starter/#11 Iteration: The for Loop/script.js
+++ 02-Fundamentals-Part-2/starter/#11 Iteration: The for Loop/script.js
@@ -1,3 +1,23 @@
 "use strict"; // for avoiding bugs
 
 // #11 Iteration: The for Loop
+
+// Loops are control structures  which allow us to automate repetitive tasks
+
+// Repetition with hard coding
+console.log("Lifting weights repetition 1 🏋️‍♀️");
+console.log("Lifting weights repetition 2 🏋️‍♀️");
+console.log("Lifting weights repetition 3 🏋️‍♀️");
+console.log("Lifting weights repetition 4 🏋️‍♀️");
+console.log("Lifting weights repetition 5 🏋️‍♀️");
+console.log("Lifting weights repetition 6 🏋️‍♀️");
+console.log("Lifting weights repetition 7 🏋️‍♀️");
+console.log("Lifting weights repetition 8 🏋️‍♀️");
+console.log("Lifting weights repetition 9 🏋️‍♀️");
+console.log("Lifting weights repetition 10 🏋️‍♀️");
+
+// Repetition with a 'for loop'
+// For loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
+}
