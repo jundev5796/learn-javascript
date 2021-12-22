646451db085f74a6309fed57aca1119587c2703e
HyunJun Baek <hjbaek91@gmail.com>
2021-12-22 15:13:38 +0900

    Configuring VSCode Settings + Prettier

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+// using the 'Prettier' tool automatically structures our code to look more consistent.
+// we can adjust the formatting rules of 'Prettier' by creating a separate configuration file and configuring preferrable settings.
+const x = 23;
+if (x === 23) console.log(23);
+
+// getting rid of brackets around a function variable.
+const calcAge = birthYear => 2037 - birthYear;
+
+// configuring 'snippets' to avoid writing repetitive commands.
+// Ex) changing the shortcut command of 'console.log()' to 'cl'.
+console.log();
