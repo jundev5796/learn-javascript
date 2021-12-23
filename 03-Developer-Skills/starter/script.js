046df97b7ae1f611b295a0b2945b31e254461614
HyunJun Baek <hjbaek91@gmail.com>
2021-12-23 16:04:19 +0900

    successfully learning how to code

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -18,3 +18,50 @@ console.log();
 // testing the 'live server' tool.
 // automatically computes and updates result on the chrome developer tool.
 console.log(calcAge(1995));
+
+// --SUCCESSFULLY LEARNING HOW TO CODE--
+
+// Have a Clear Goal
+
+// 1. Set a specific, measurable, realistic and time-based goal.
+// 2. Know exactly why you are learning to code.
+// 3. Imagine a big project you want to be able to build.
+// 4. Research technologies you need and then learn them: JavaScript, React, etc.
+
+// Actually try to Code
+
+// 1. Understand the code that you're studying and typing.
+// 2. Always type the code, don't just copy-paste.
+
+// Reinforce your Learning
+
+// 1. After you learn a new feature or concept, apply it immediately.
+// 2. Take notes: hand-written, blog.
+// 3. Challenge yourself and practice with small coding exercises and challenges.
+// 4. Do not hurry in trying to learn something quick. Everyone has their own learning pace.
+
+// Keep Practicing
+
+// 1. Practicing on your own is the MOST important thing do do. Without practice, you will not get anywhere!
+// 2. Come up with your own project ideas or copy popular sites or applications, or just parts of them in the beginning.
+// 3. Do not become used to getting help from others all the time.
+
+// Do not become Frustrated
+
+// 1. Don't get stuck trying to write the perfect code. Just make sure the code works. Clean and efficient code will come with time.
+// 2. No one is perfect and practice will help you become better.
+// 3. If you have time, try refactoring your code.
+
+// Don't Give Up!
+
+// 1. Embrace the fact that you will never know everything.
+// 2. Just focus on what you need to achieve your goal.
+// 3. Even the best developers today continue to learn and make mistakes.
+
+// Work with Others!
+
+// 1. Work together with others and reinforce your knowledge.
+// 2. Try explaining your knowledge to others.
+// 3. You could even try to build a group project with others.
+
+// Source: Jonas Schmedtmann
