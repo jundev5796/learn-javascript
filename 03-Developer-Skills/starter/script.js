d90c0668245cf4986444bfdc9d8555fba533ed72
HyunJun Baek <hjbaek91@gmail.com>
2021-12-25 16:28:22 +0900

    Using the debugger tool to fix a bug

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -194,7 +194,8 @@ const measureKelvin = function () {
     unit: 'celsius',
 
     // C) FIX
-    value: Number(prompt('Degrees celsius:')), // the 'prompt' function always returns a 'string'
+    // value: Number(prompt('Degrees celsius:')), // the 'prompt' function always returns a 'string'
+    value: 10,
   };
 
   // B) FIND
@@ -207,3 +208,31 @@ const measureKelvin = function () {
 };
 // A) IDENTIFY
 console.log(measureKelvin());
+
+// Using a Debugger
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    debugger;
+    if (curTemp > max) {
+      max = curTemp;
+    }
+    if (curTemp < min) {
+      min = curTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+
+// A) IDENTIFY
+console.log(amplitudeBug);
