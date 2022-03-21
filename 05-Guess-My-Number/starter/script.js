26b2eecd9577d49d6f538b70061efad676129902
HyunJun Baek <hjbaek91@gmail.com>
2022-03-21 13:53:05 +0900

    PROJECT #1: Guess My Number! + Reading Contents using DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,5 @@
 'use strict';
+
+// selecting 'class' or 'id' from javascript (similar with css)
+// reading the text conetent using the 'textContent' property
+console.log(document.querySelector('.message').textContent);
