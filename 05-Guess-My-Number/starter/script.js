bcd6053c11f589c0d04a773c05ba7a263956f2f8
HyunJun Baek <hjbaek91@gmail.com>
2022-03-21 14:03:59 +0900

    DOM and DOM Manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// What is the DOM?
+// - DOM stands for 'Document Object Model' which is a structured representation of HTML documents.
+// - Allows JavaScript to access HTML elements and styles to manipulate them.
+// - (Ex) changes text, HTML attributes, and even CSS styles.
+// - They are API's that can be written in JavaScript and applied in the broswer (DOM !== JavaScript).
+
 // selecting 'class' or 'id' from javascript (similar with css)
 // reading the text conetent using the 'textContent' property
 console.log(document.querySelector('.message').textContent);
