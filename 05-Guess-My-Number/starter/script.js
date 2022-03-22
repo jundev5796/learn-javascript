b62de4c0f07424019e698b17eaf77b816132c02a
HyunJun Baek <hjbaek91@gmail.com>
2022-03-22 14:52:02 +0900

    Implementing the Game Logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,14 +19,39 @@ console.log(document.querySelector('.message').textContent);
 // document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 
+// creating an initial 'secret number' for the game logic
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 // an 'event' is something that happens from a certain action (ex. mouse-click, cursor-moving, button-press)
 // an 'event listener' is when we are waiting for a certain action to happen
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
-  // in the case where there is no input
+  // in the case where there is no input, equal to, higher, or lower
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
