fd17fa71702603ffcd5d8699096d094a7d0c0bbd
HyunJun Baek <hjbaek91@gmail.com>
2022-03-22 14:24:57 +0900

    explanation for conditional statement for no input

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -25,6 +25,7 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // in the case where there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
   }
