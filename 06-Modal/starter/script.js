683f60286852b760dbb16382e47d5342afa624ad
HyunJun Baek <hjbaek91@gmail.com>
2022-03-25 14:56:52 +0900

    Handling Keypress Events

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -24,7 +24,15 @@ const closeModal = function () {
 
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', openModal);
-
-  btnCloseModal.addEventListener('click', closeModal);
-  overlay.addEventListener('click', closeModal);
 }
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+// handling keypress events
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
