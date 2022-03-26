24b03bc8d0e1c1eec813e239a72a5a6ed01b9bb6
HyunJun Baek <hjbaek91@gmail.com>
2022-03-26 21:41:38 +0900

    PROJECT #3: Pig Game + Selecting id

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// selecting elements
+const score0El = document.querySelector('#score--0');
+
+// shortcut to selecting id ('getElementById)
+const score1El = document.getElementById('score--1');
+
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
