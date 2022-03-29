64fbaf4d29384552473630b057a7cecb0058ae00
HyunJun Baek <hjbaek91@gmail.com>
2022-03-29 16:21:45 +0900

    An High-Level Overview of JavaScript

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,12 @@
-'use strict';
+"use strict";
+
+// --JAVASCRIPT OVERVIEW--
+// 1. High-level
+// 2. Garbage-collected
+// 3. Interpreted or just-in-time compiled
+// 4. Multi-paradigm
+// 5. Prototype-based object-oriented
+// 6. First-class functions
+// 7. Dynamic
+// 8. Single-threaded
+// 9. Non-blocking event loop
