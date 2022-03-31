378205b963d5a48a6c628719b1578c0a713c522e
HyunJun Baek <hjbaek91@gmail.com>
2022-03-31 15:43:00 +0900

    Comparing let, const, and var in Hoisting

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -151,4 +151,48 @@ calcAge(1991);
 // - Using functions before actual declaration
 // - 'var' hoisting is just a byproduct
 
+// --EXAMPLES--
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'John';
+let job = 'Programmer';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// --EXAMPLES(2)--
+
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+// --EXAMPLES(3)--
+
+// 'let' and 'const' do not create properties on the window object
+var x = 1;
+let y = 2;
+const z = 3;
+
 // ------------------------
