d3e1e25e32d91dfa25a0384e89eb1ef4ae09934b
HyunJun Baek <hjbaek91@gmail.com>
2022-04-03 17:48:32 +0900

    Setting Default Values during Destructuring

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -67,3 +67,7 @@ const nested = [2, 4, [5, 6]];
 // console.log(i, j);
 const [i, , [j, k]] = nested;
 console.log(i, j, k);
+
+// default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
