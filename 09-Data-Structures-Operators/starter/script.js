5c48075b28cf9c7a92f8bf3f68e85287c0eecd10
HyunJun Baek <hjbaek91@gmail.com>
2022-04-04 10:01:26 +0900

    Calling a Function and Destructuring Objects at the same time

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -26,10 +26,24 @@ const restaurant = {
       close: 24,
     },
   },
+
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
 
 // destructuring objects
 const { name, openingHours, categories } = restaurant;
