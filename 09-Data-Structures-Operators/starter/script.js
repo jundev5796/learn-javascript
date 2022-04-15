c78412716e6d651bad05fdb90005a3cebf4b30da
HyunJun Baek <hjbaek91@gmail.com>
2022-04-15 16:33:56 +0900

    Maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -56,6 +56,55 @@ const restaurant = {
 };
 
 ////////////////////////////////////////////
+// --Maps: Fundamentals--
+
+// maps are data structures stored as key-value pairs (just like objects)
+// unlike objects, keys in maps can be any type
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano'); // key-value
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal')); // updates and returns values
+
+// chaining multiple key-values
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+// reading data from map
+console.log(rest.get('name'));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+// example
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// checking if a map contains a certain key
+console.log(rest.has('categories'));
+
+// deleting elements on maps
+rest.delete(2);
+console.log(rest);
+
+// checking number of elements on maps
+console.log(rest.size);
+
+// using arrays or objects as map keys
+const arr = [1, 2];
+rest.set(arr, 'Test');
+
+// applying maps on DOM
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+
+console.log(rest.get(arr));
+
+/*
+////////////////////////////////////////////
 // --Sets (collection of unique values without duplicates)--
 
 // There is no need to retrieve values out of a set
@@ -100,6 +149,7 @@ console.log(
 );
 
 console.log(new Set('John').size);
+*/
 
 /*
 ////////////////////////////////////////////
