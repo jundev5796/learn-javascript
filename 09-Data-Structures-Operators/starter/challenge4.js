833e1ef1539ce347795c9476a15ec02ae8fcbb03
HyunJun Baek <hjbaek91@gmail.com>
2022-04-18 18:13:34 +0900

    Coding Challenge #4

---
+++ 09-Data-Structures-Operators/starter/challenge4.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// Test Data
+/*
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+*/
+
+/*
+underscore_case -> underscoreCase
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+// Solution
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log(rows);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
