b2d796f5628863b3b07a8b54273ef2d5b246b358
HyunJun Baek <hjbaek91@gmail.com>
2022-06-04 18:48:28 +0900

    updated notes on logical assignment operator with short circuiting

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -52,6 +52,41 @@ const restaurant = {
   },
 };
 
+// 7. Logical Assignment Operators
+
+// example
+const rest1 = {
+  name: 'Capri',
+  numGuests: 20,
+};
+
+const rest2 = {
+  name: 'Capri',
+  owner: 'Giovanni Rossi',
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+// rest1.numGuests ||= 10;
+// rest2.numGuests ||= 10;
+
+// for cases where the property is 0
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+// AND assignment operator
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+rest1.owner &&= '<ANONYMOUS>';
+rest2.woner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+// --
+
 // 6. The Nullish Coalescing Operator (??)
 
 restaurant.numGuests = 0;
