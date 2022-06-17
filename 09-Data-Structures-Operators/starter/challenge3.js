5c3a13de38c8e63edbc9310403da2d160a3083df
HyunJun Baek <hjbaek91@gmail.com>
2022-06-17 22:03:42 +0900

    updated challenge

--- 09-Data-Structures-Operators/starter/challenge3.js
+++ 09-Data-Structures-Operators/starter/challenge3.js
@@ -24,11 +24,31 @@ console.log(gameEvents);
 
 // 3.
 console.log(
-  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+  `An event happened, on average every ${90 / gameEvents.size} minutes`
 );
 
 // 4.
 for (const [min, event] of gameEvents) {
-  const half = min <= 45 ? 'FIRST' : 'SECOND';
-  console.log(`[${half} HALF] ${min}: ${event}`);
+  console.log(`[${min <= 45 ? 'FIRST' : 'SECOND'} HALF] ${min}: ${event}`);
 }
+
+// ------------------------------------------------------------------------------
+
+// // 1.
+// const events = [...new Set(gameEvents.values())];
+// console.log(events);
+
+// // 2.
+// gameEvents.delete(64);
+// console.log(gameEvents);
+
+// // 3.
+// console.log(
+//   `An event happened, on average, every ${90 / gameEvents.size} minutes`
+// );
+
+// // 4.
+// for (const [min, event] of gameEvents) {
+//   const half = min <= 45 ? 'FIRST' : 'SECOND';
+//   console.log(`[${half} HALF] ${min}: ${event}`);
+// }
