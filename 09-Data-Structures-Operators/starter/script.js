6d7d36709dce31d5076786668ed42c4ac95cf931
HyunJun Baek <hjbaek91@gmail.com>
2022-06-21 06:09:04 +0900

    updated notes on string

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,6 +54,65 @@ const restaurant = {
   },
 };
 
+// 16. Working With Strings #3
+
+console.log(
+  '---------------------Working With Strings #3---------------------'
+);
+
+// split method
+console.log('a+very+nice+string'.split('+'));
+console.log('John Smith'.split(' '));
+
+// join method
+const [firstName, lastName] = 'John Smith'.split(' ');
+
+const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizeName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice(1));
+  }
+  console.log(namesUpper.join(' '));
+};
+
+capitalizeName('jessica ann smith davis');
+capitalizeName('john smith');
+
+// padding a string (to add number of characters until the string has a certain desired length)
+const message = 'Go to gate 23!';
+console.log(message.padStart(20, '+').padEnd(30, '+'));
+console.log('John'.padStart(20, '+').padEnd(30, '+'));
+
+// EXAMPLE (padding)
+const maskCreditCard = function (num) {
+  const str = num + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(29082930480));
+console.log(maskCreditCard(43378463864647384));
+console.log(maskCreditCard('029372075894257239048'));
+
+// repeat method
+const message2 = 'Bad weather... All Departures Delayed... ';
+console.log(message2.repeat(5));
+
+const planesInLine = function (n) {
+  console.log(`There are ${n} planes in line ${'P'.repeat(n)}`);
+};
+
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+// --
+
 // 15. Working With Strings #2
 
 console.log(
