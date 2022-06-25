85fc2b14f8a891987676d0e981229d539940e28a
HyunJun Baek <hjbaek91@gmail.com>
2022-06-26 06:27:33 +0900

    updated challenge

--- 09-Data-Structures-Operators/starter/challenge4.js
+++ 09-Data-Structures-Operators/starter/challenge4.js
@@ -24,7 +24,6 @@ document.body.append(document.createElement('button'));
 document.querySelector('button').addEventListener('click', function () {
   const text = document.querySelector('textarea').value;
   const rows = text.split('\n');
-  console.log(rows);
 
   for (const [i, row] of rows.entries()) {
     const [first, second] = row.toLowerCase().trim().split('_');
@@ -35,3 +34,20 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
+
+// --
+
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
+//   console.log(rows);
+
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+//     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+//   }
+// });
