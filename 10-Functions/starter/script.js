b2d89dbbb70222ef24d439afb018871cc35f0e83
HyunJun Baek <hjbaek91@gmail.com>
2022-06-27 10:00:01 +0900

    passing arguments with values and references

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -30,3 +30,44 @@ createBooking("LH123", 2, 800);
 createBooking("LH123", 2);
 createBooking("LH123", 5);
 createBooking("LH123", undefined, 1000); // skipping a value
+
+// --
+
+// 2. How Passing Arguments Works: Value vs. Reference
+
+console.log(
+  "--------------------------How Passing Arguments Works: Value vs. Reference--------------------------"
+);
+
+// - primitive types gets copied while not affecting the original
+// - reference types gets copied (from the heap) which can affect the original
+// - javascript only passes by value NOT reference
+
+const flight = "LH234";
+const john = {
+  name: "John Smith",
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = "LH999";
+  passenger.name = "Mr. " + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert("Checked in");
+  } else {
+    alert("Wrong passport!");
+  }
+};
+
+checkIn(flight, john);
+console.log(flight);
+console.log(john);
+
+// EXAMPLE
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(john);
+checkIn(flight, john);
