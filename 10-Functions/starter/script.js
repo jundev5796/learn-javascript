ac6fec71787b9ab8f24aafae1e7be89d9d9b9237
HyunJun Baek <hjbaek91@gmail.com>
2022-07-04 09:05:26 +0900

    closure challenge

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -375,3 +375,66 @@ booker();
 
 // looking into closure property
 console.dir(booker);
+
+// --
+
+// More Closure Examples
+
+console.log(
+  "--------------------------More Closure Examples--------------------------"
+);
+
+// example #1
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f(); // the 'f' function closed over the 'g' function
+
+// re-assigning 'f' function (reborn on a different function)
+h();
+f();
+console.dir(f);
+
+// example #2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+
+// --
+
+// Challenge #2
+
+console.log("--------------------------Challenge #2--------------------------");
+
+(function () {
+  const header = document.querySelector("h1");
+  header.style.color = "red";
+
+  document.querySelector("body").addEventListener("click", function () {
+    header.style.color = "blue";
+  });
+})();
