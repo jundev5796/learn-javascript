46cbe100c10f63bd71bfee04eefd9cf0b0e5174a
HyunJun Baek <hjbaek91@gmail.com>
2022-07-11 07:21:55 +0900

    challenge #4

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -436,3 +436,21 @@ const totalDepositsUSD = movements
   .map((mov) => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD);
+
+// --
+
+// Challenge #3
+
+console.log(
+  "-------------------------------Challenge #3-------------------------------"
+);
+
+const calcAverageHumanAgeArrow = (ages) =>
+  ages
+    .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter((age) => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+const avg3 = calcAverageHumanAgeArrow([5, 2, 4, 1, 15, 8, 3]);
+const avg4 = calcAverageHumanAgeArrow([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg3, avg4);
