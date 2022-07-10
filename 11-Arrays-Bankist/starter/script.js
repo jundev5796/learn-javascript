e214ccd6fbe1bb0debb895fbbb9b7e550b0671e7
HyunJun Baek <hjbaek91@gmail.com>
2022-07-11 06:54:48 +0900

    the magic of chaining methods

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -72,7 +72,7 @@ const displayMovements = function (movements) {
       <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">${mov}</div>
+      <div class="movements__value">${mov}€</div>
     </div>`;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
@@ -90,9 +90,36 @@ console.log(
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${balance} EUR`;
+  labelBalance.textContent = `${balance}€`;
 };
 calcDisplayBalance(account1.movements);
+
+// --
+
+const calcDisplaySummary = function (movements) {
+  const incomes = movements
+    .filter((mov) => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes}€`;
+
+  const out = movements
+    .filter((mov) => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = movements
+    .filter((mov) => mov > 0)
+    .map((deposit) => (deposit * 1.2) / 100)
+    .filter((int, i, arr) => {
+      console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest}€`;
+};
+calcDisplaySummary(account1.movements);
+
+// --
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -341,6 +368,7 @@ console.log(
 // - boil down all the elements in an array into a single value
 // - unlike other methods, the value of the reduce method acts as an 'accumulator' (accumulates like a snowball)
 // - the 'second parameter' (0) at the end indicates the initial value
+// - the 'reduce' method always returns a value (not an array)
 
 console.log(movements);
 
@@ -391,3 +419,20 @@ const calcAverageHumanAge = function (ages) {
 const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
 const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
+
+// --
+
+// 9. The Magic of Chaining Methods
+
+console.log(
+  "-------------------------------The Magic of Chaining Methods-------------------------------"
+);
+
+// - chaining all array methods
+
+// pipeline
+const totalDepositsUSD = movements
+  .filter((mov) => mov > 0)
+  .map((mov) => mov * eurToUsd)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
