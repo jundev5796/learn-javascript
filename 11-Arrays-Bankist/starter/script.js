6452d1141c631024eb066a51f035b860cedeebb4
HyunJun Baek <hjbaek91@gmail.com>
2022-07-21 16:02:11 +0900

    challenge #4-3

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -826,3 +826,14 @@ console.log(
     dogSarah.curFood > dogSarah.recFood ? "much" : "little"
   }`
 );
+
+// 3.
+const ownersEatTooMuch = dogs
+  .filter((dog) => dog.curFood > dog.recFood)
+  .flatMap((dog) => dog.owners);
+console.log(ownersEatTooMuch);
+
+const ownersEatTooLittle = dogs
+  .filter((dog) => dog.curFood < dog.recFood)
+  .flatMap((dog) => dog.owners);
+console.log(ownersEatTooLittle);
