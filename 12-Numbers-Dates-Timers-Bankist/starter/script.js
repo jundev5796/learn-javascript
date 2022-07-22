b9579b415271efd16a65ca138dac6cda0942cbdd
HyunJun Baek <hjbaek91@gmail.com>
2022-07-23 08:47:29 +0900

    the remainder operator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -343,3 +343,42 @@ console.log((2.7).toFixed(0)); // returns a string
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.345).toFixed(2));
+
+// --
+
+// 3. The Remainder Operator
+
+console.log(
+  "-------------------------------The Remainder Operator-------------------------------"
+);
+
+// - returns the remainder of a division
+
+console.log(5 % 2); // remainder
+console.log(5 / 2); // 5 = 2 * 2 + 1
+
+console.log(8 % 3);
+console.log(8 / 3); // 8 = 2 * 3 + 2
+
+// odd / even
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+// example #1
+const isEven = (n) => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(23));
+console.log(isEven(514));
+
+// example #2
+labelBalance.addEventListener("click", function () {
+  [...document.querySelectorAll(".movements__row")].forEach(function (row, i) {
+    // 0, 2, 4, 6
+    if (i % 2 === 0) row.style.backgroundColor = "orangered";
+    // 0, 3, 6, 9
+    if (i % 3 === 0) row.style.backgroundColor = "blue";
+  });
+});
